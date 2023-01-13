9f9781349ad97d17e69427d603e565e107bd02cb
arkmadj <ahmadjinadu@gmail.com>
2023-01-13 22:43:48 +0100

    Addedd some contents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div>Welcome to the homepage</div>
+    <div>
+      <h1 className="text-4xl">Welcome to my blog</h1>
+      <p>Some content</p>
+    </div>
   )
 }
 
