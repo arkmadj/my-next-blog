c2e609afe93b73235fdce381d34342be03f6aa61
arkmadj <ahmadjinadu@gmail.com>
2023-01-13 23:23:12 +0100

    Updated sanity cli

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,12 @@
 import {defineCliConfig} from 'sanity/cli'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
+
 export default defineCliConfig({
   api: {
-    projectId: '4x9yi8lv',
-    dataset: 'production'
+    projectId,
+    dataset
   }
 })
