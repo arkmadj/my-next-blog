c4c3ba6a1dff5129e8696240d71fb8d257005401
arkmadj <ahmadjinadu@gmail.com>
2023-01-13 23:17:17 +0100

    Added env variables

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,15 @@ import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
 export default defineConfig({
 	name: "default",
 	title: "nextjs13 sanity blog",
 
-	projectId: "4x9yi8lv",
-	dataset: "production",
+	projectId,
+	dataset,
 
 	plugins: [deskTool(), visionTool()],
 
