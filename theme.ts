ea1803ad7e491b3075c06693799daea95dcf6038
arkmadj <ahmadjinadu@gmail.com>
2023-01-14 21:00:47 +0100

    Added sanity theme file

--- theme.ts
+++ theme.ts
@@ -1,10 +1,10 @@
 import { buildLegacyTheme } from "sanity";
 
 const props = {
-  "--my-white":"#fff",
-  "--my-black":"#1a1a1a",
-  "--zultan-brand":"#772CE8",
-  "--my-red":"#db4437",
-  "--my-yellow":"#f4b400",
-  "--my-green":"#0f9d58",
-}
+	"--my-white": "#fff",
+	"--my-black": "#1a1a1a",
+	"--zultan-brand": "#772CE8",
+	"--my-red": "#db4437",
+	"--my-yellow": "#f4b400",
+	"--my-green": "#0f9d58",
+};
