61e2fe8304b0cf205ce22a744418ac097751dc98
arkmadj <ahmadjinadu@gmail.com>
2023-01-14 21:07:35 +0100

    Added sanity theme file

--- theme.ts
+++ theme.ts
@@ -8,3 +8,26 @@ const props = {
 	"--my-yellow": "#f4b400",
 	"--my-green": "#0f9d58",
 };
+
+export const myTheme = buildLegacyTheme({
+  // Base theme colors
+  "--black": props["--my-black"],
+  "--white": props["--my-white"],
+  
+  "--gray": "#666",
+  "--gray-base": "#666",
+
+  "--component-bg": props["--my-black"],
+  "--component-text-color": props["--my-white"],
+  
+  // Brand
+  "--brand-primary": props["--zultan-brand"],
+  
+  // Default
+  "--default-button-color": "#666",
+  "--default-button-primary-color": props["--zultan-brand"],
+  "--default-button-success-color": props["--my-green"],
+  "--default-button-warning-color": props["--my-yellow"],
+  "--default-button-danger-color": props["--my-red"],
+
+})
