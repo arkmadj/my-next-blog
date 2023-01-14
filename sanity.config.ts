494e07a930ed69e6e9a8b1b86c45d7423ac1032b
arkmadj <ahmadjinadu@gmail.com>
2023-01-14 22:00:40 +0100

    Added studion nav bar

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,7 @@ import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 import { myTheme } from "./theme";
+import StudioNavbar from "@/components/StudioNavbar";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -16,6 +17,12 @@ export default defineConfig({
 	plugins: [deskTool(), visionTool()],
 	schema: {
 		types: schemaTypes,
+	},
+	studio: {
+		components: {
+			// logo: Logo,
+			navbar: StudioNavbar,
+		}
 	},
 	theme: myTheme,
 });
