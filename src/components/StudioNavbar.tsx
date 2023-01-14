d6f26eb9285600a2b6a6bd5a888a84f84041f0a0
arkmadj <ahmadjinadu@gmail.com>
2023-01-14 22:33:36 +0100

    Added Studio nav bar

--- src/components/StudioNavbar.tsx
+++ src/components/StudioNavbar.tsx
@@ -7,7 +7,7 @@ const StudioNavbar = (props: NavbarProps) => {
 		<div>
 			<div className="flex items-center justify-between p-5">
 				<Link href="/" className="text-[#772CE8] flex items-center">
-          {/* <ArrowUturnLeftIcon className="h-6 w-6 text-[#772CE8] mr-2"/> */}
+          <ArrowUturnLeftIcon className="h-6 w-6 text-[#772CE8] mr-2"/>
 					Go to website
 				</Link>
 			</div>
