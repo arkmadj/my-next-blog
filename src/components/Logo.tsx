f42baeeea6354e08b36dfb71d89b4e749109dbcb
arkmadj <ahmadjinadu@gmail.com>
2023-01-14 23:17:22 +0100

    Added studio logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { LogoProps } from 'sanity'
+
+const Logo = (props: LogoProps) => {
+  const {renderDefault, title } = props;
+  return (
+    <div>
+      <img src="" alt="" className="roounded-full" />
+    </div>
+  )
+}
+
+export default Logo
