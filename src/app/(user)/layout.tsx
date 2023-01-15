8a6167c5bfc6cdf7e1a650956fadc67abcc903b7
arkmadj <ahmadjinadu@gmail.com>
2023-01-15 23:09:08 +0100

    Added banner

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,3 +1,4 @@
+import Banner from "@/components/Banner";
 import Header from "@/components/Header";
 import "../globals.css";
 
@@ -10,6 +11,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body>
 				<Header />
+        <Banner/>
 				{children}
 			</body>
 		</html>
