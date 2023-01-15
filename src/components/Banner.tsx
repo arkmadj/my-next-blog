70ec3f5a7c7361b7758df18f39ed255ad4c69b74
arkmadj <ahmadjinadu@gmail.com>
2023-01-15 23:09:12 +0100

    Added banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,9 +2,15 @@ const Banner = () => {
 	return (
 		<div>
 			<div>
-        <h1 className="text-7xl">Zultans daily blog</h1>
-        <h2 className="mt-5 md:mt-0">Welcome to {"zultan"}</h2>
-      </div>
+				<h1 className="text-7xl">Zultans daily blog</h1>
+				<h2 className="mt-5 md:mt-0">
+					Welcome to {" "}
+					<span className="underline decoration-4 decoration-[#772CE8]">
+						Every developer
+					</span>{" "} 
+          favorite blog in the Devosphere
+				</h2>
+			</div>
 			<p className="max-w-sm mt-5 text-gray-400 md:mt-2">
 				New product features | The latest in technology | The weekly debugging
 				nightmares & more!
