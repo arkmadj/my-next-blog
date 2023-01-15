310d1ab4c2c5f78bca3884bd93b73e7e7a85846c
arkmadj <ahmadjinadu@gmail.com>
2023-01-15 22:42:19 +0100

    Added image and link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,24 @@
-
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
-  return (
-    <header className="flex items-center justify-between px-10 py-5 space-x-2 font-bold">
-      <div></div>
-      <div></div>
-    </header>
-  )
-}
+	return (
+		<header className="flex items-center justify-between px-10 py-5 space-x-2 font-bold">
+			<div>
+				<Link href="/">
+					<Image
+						className="rounded-full"
+						height={50}
+						width={50}
+						src="https://resources.premierleague.com/premierleague/badges/50/t3.png"
+            alt="logo"
+					/>
+				</Link>
+        <h1>Zultan</h1>
+			</div>
+			<div></div>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
