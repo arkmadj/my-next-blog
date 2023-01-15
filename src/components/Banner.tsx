88e8dc949c10ad841639c8ff4fe069824198b2db
arkmadj <ahmadjinadu@gmail.com>
2023-01-15 23:06:32 +0100

    Added banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,16 @@
+const Banner = () => {
+	return (
+		<div>
+			<div>
+        <h1 className="text-7xl">Zultans daily blog</h1>
+        <h2 className="mt-5 md:mt-0">Welcome to {"zultan"}</h2>
+      </div>
+			<p className="max-w-sm mt-5 text-gray-400 md:mt-2">
+				New product features | The latest in technology | The weekly debugging
+				nightmares & more!
+			</p>
+		</div>
+	);
+};
+
+export default Banner;
