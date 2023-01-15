99ab56ded4f0d5b15143319ee4740725ff41b883
arkmadj <ahmadjinadu@gmail.com>
2023-01-15 21:07:08 +0100

    Added logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,21 @@
-import Image from 'next/image';
-import React from 'react'
-import { LogoProps } from 'sanity'
+import Image from "next/image";
+import React from "react";
+import { LogoProps } from "sanity";
 
 const Logo = (props: LogoProps) => {
-  const {renderDefault, title } = props;
-  return (
-    <div>
-      <Image src="" height={50} width={50} alt="zultan logo" className="roounded-full" />
-      {renderDefault && <>{renderDefault(props)}</>}
-    </div>
-  )
-}
+	const { renderDefault, title } = props;
+	return (
+		<div>
+			<Image
+				src="https://resources.premierleague.com/premierleague/badges/50/t3.png"
+				height={50}
+				width={50}
+				alt="zultan logo"
+				className="roounded-full"
+			/>
+			<>{renderDefault(props)}</>
+		</div>
+	);
+};
 
-export default Logo
+export default Logo;
