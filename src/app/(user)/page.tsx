e4196fece84502765a4ba047624308a8170ddeb6
arkmadj <ahmadjinadu@gmail.com>
2023-01-15 21:21:59 +0100

    added user group

---
+++ src/app/(user)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className="text-4xl text-red-500">Welcome to my blog</h1>
+      <p>Some content</p>
+    </div>
+  )
+}
+
+export default HomePage
