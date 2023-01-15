5ac9cf1a6f621898f70e5c66958eb8fe34b7faea
arkmadj <ahmadjinadu@gmail.com>
2023-01-15 23:11:54 +0100

    Added banner

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 function HomePage() {
   return (
     <div>
-      <h1 className="text-4xl text-red-500">Welcome to my blog</h1>
-      <p>Some content</p>
     </div>
   )
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,14 @@
 const Banner = () => {
 	return (
-		<div>
+		<div className="flex flex-col justify-between px-10 py-5 mb-10 font-bold lg:flex-row lg:space-x-5">
 			<div>
 				<h1 className="text-7xl">Zultans daily blog</h1>
 				<h2 className="mt-5 md:mt-0">
-					Welcome to {" "}
+					Welcome to{" "}
 					<span className="underline decoration-4 decoration-[#772CE8]">
 						Every developer
-					</span>{" "} 
-          favorite blog in the Devosphere
+					</span>{" "}
+					favorite blog in the Devosphere
 				</h2>
 			</div>
 			<p className="max-w-sm mt-5 text-gray-400 md:mt-2">
