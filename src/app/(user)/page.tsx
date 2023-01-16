69c801a9e59abc0262756dc0d6bad2bae005d947
arkmadj <ahmadjinadu@gmail.com>
2023-01-17 00:03:41 +0100

    import preview suspense

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { previewData } from 'next/headers'
-import { groq } from 'next-sanity'
-import { client } from 'lib/sanity.client'
-import PreviewSuspense from "@/components/PreviewSuspense"
+import React from "react";
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "lib/sanity.client";
+import PreviewSuspense from "@/components/PreviewSuspense";
 
 const query = groq`
   *[_type=='post']{
@@ -10,19 +10,31 @@ const query = groq`
     author,
     categories[]->
   } | order(_createdAt desc)
-`
+`;
 
 async function HomePage() {
-  if(previewData()){
-    return <div>Preview mode</div>
-  }
+	if (previewData()) {
+		return (
+			<PreviewSuspense
+				fallback={
+					<div role="status">
+						<p className="text-center text-lg animate-pulse text-[#772CE8]">
+							Loading Preview Data...
+						</p>
+					</div>
+				}
+			>
+				Preview mode
+			</PreviewSuspense>
+		);
+	}
 
-  const posts = await client.fetch(query)
-  return (
-    <div>
-      <h1>Not in preview mode</h1>
-    </div>
-  )
+	const posts = await client.fetch(query);
+	return (
+		<div>
+			<h1>Not in preview mode</h1>
+		</div>
+	);
 }
 
-export default HomePage
+export default HomePage;
