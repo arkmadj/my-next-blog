3eee26ebcd45d5236210ad3179d9cc9d2af3095b
arkmadj <ahmadjinadu@gmail.com>
2023-01-16 23:20:59 +0100

    Added onPublic acess function

--- lib/sanity.preview.ts
+++ lib/sanity.preview.ts
@@ -6,3 +6,9 @@ import { projectId, dataset } from "./sanity.client"
 function onPublicAccessOnly(){
   throw new Error(`Unable to load preview as you're not logged in`)
 }
+
+if(!projectId || dataset){
+  throw new Error(
+    `Missing projectId or dataset. Check your sanity.json or .env`
+  )
+}
