38d81b46253c9dbbc79ba1d8ff2f5329460795d3
arkmadj <ahmadjinadu@gmail.com>
2023-01-16 23:47:58 +0100

    Added preview data

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { PreviewData } from 'next'
 
 function HomePage() {
+  if(PreviewData()){
+    return <div>Preview mode</div>
+  }
   return (
     <div>
+      
     </div>
   )
 }
