4cc14b48afb418222115b864cd1d1ef72dbc5789
arkmadj <ahmadjinadu@gmail.com>
2023-01-17 23:33:26 +0100

    Added block interface

--- typings.d.ts
+++ typings.d.ts
@@ -8,43 +8,54 @@ type Base = {
 	_updatedAt: string;
 };
 
-interface Post extends Base{
-  author: AuthStoreOptions;
-  body: Block[];
-  categories: Category[];
-  mainImage: Image;
-  slug: Slug;
-  title: string;
-  description: string;
+interface Post extends Base {
+	author: AuthStoreOptions;
+	body: Block[];
+	categories: Category[];
+	mainImage: Image;
+	slug: Slug;
+	title: string;
+	description: string;
 }
 
 interface Author extends base {
-  bio: Block[];
-  image: Image;
-  name: string;
-  slug: Slug;
+	bio: Block[];
+	image: Image;
+	name: string;
+	slug: Slug;
 }
 
 interface Image {
-  _type: "image";
-  asset: Reference;
+	_type: "image";
+	asset: Reference;
 }
 
 interface Reference {
-  _ref: string;
-  _type: "reference";
+	_ref: string;
+	_type: "reference";
 }
 
 interface Slug {
-  _type: "slug";
-  current: 
-  string;
+	_type: "slug";
+	current: string;
 }
 
 interface Block {
-  _key: string;
-  _type: "block";
-  children: Span[]
-  markDefs: any[]
-  style: "normal" | "h1" | "h2" | "h3" | "h4" | "blockquote";
+	_key: string;
+	_type: "block";
+	children: Span[];
+	markDefs: any[];
+	style: "normal" | "h1" | "h2" | "h3" | "h4" | "blockquote";
+}
+
+interface Span {
+	_key: string;
+	_type: "span";
+	marks: string[];
+	text: string;
+}
+
+interface Category extends Base {
+	description: string;
+	title: string;
 }
