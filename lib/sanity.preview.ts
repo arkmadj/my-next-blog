4871f9e182ab0a06ff7ab410d71dac1a351a8a01
arkmadj <ahmadjinadu@gmail.com>
2023-01-18 23:49:32 +0100

    FIxed typo

--- lib/sanity.preview.ts
+++ lib/sanity.preview.ts
@@ -11,7 +11,7 @@ function onPublicAccessOnly(){
 
 if(!projectId || !dataset){
   throw new Error(
-    `Missing projectId or datdaset. Check your sanity.json or .env`
+    `Missing projectId or dataset. Check your sanity.json or .env`
   )
 }
 
