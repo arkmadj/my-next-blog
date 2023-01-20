3854ec82ffeabb2697f486b48d6806217526264d
arkmadj <ahmadjinadu@gmail.com>
2023-01-20 23:56:25 +0100

    Added next image

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -11,13 +11,13 @@ const BlogList = ({ posts }: Props) => {
 			<hr className="border-[#772CE8] mb-10" />
 			<div>
 				{posts.map((post) => (
-					<div key={post._id}>
-						<div>
+					<div key={post._id} className="flex flex-col cursor-pointer group:">
+						<div className="relative w-full transition-transform duration-200 ease-out h-80 drop-shadow-xl group-hover:scale-105">
 							<Image
 								className="object-cover object-left lg:object-center"
 								src={urlFor(post.mainImage).url()}
-                alt={post.author.name}
-                fill
+								alt={post.author.name}
+								fill
 							/>
 						</div>
 					</div>
