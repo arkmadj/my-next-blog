130798d6af8969f4ac3284ea9afd9f404afdaa6d
arkmadj <ahmadjinadu@gmail.com>
2023-01-20 23:43:22 +0100

    Added next image

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -7,19 +7,23 @@ type Props = {
 
 const BlogList = ({ posts }: Props) => {
 	return (
-    <div>
-      <hr className="border-[#772CE8] mb-10" />
-      <div>
-        {posts.map(post => (
-          <div key={post_id}>
-            <div>
-              <Image className="object-cover object-left lg:object-center"/>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+		<div>
+			<hr className="border-[#772CE8] mb-10" />
+			<div>
+				{posts.map((post) => (
+					<div key={post_id}>
+						<div>
+							<Image
+								className="object-cover object-left lg:object-center"
+								src={urlFor(post.mainImage).url()}
+                alt={post.author.name}
+							/>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default BlogList;
